483082762c9016dc95deaeeb279d1b3ebc9ece15
Wouter van der Plas <2423856+wvanderp@users.noreply.github.com>
2024-05-05 22:13:33 +0200

    set up netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: `/${packageJson.name}/`
+  plugins: [react()]
 })
